1bb44c06ab39d532c145b0210d04c43abf06cd71
Isabelly Barbosa <isabelly@ufam.edu.br>
2016-06-23 15:46:24 -0400

    Ajuste na funcionalidade de upload de multiplos arquivos.

--- grails-app/controllers/biblioteca_app/EstanteController.groovy
+++ grails-app/controllers/biblioteca_app/EstanteController.groovy
@@ -13,45 +13,50 @@ class EstanteController extends RestfulController {
     //
     // curl -F "image[]=@/home/dev/Downloads/th_01.gif" -F "image[]=@/home/dev/Downloads/th_02.gif"  http://localhost:8080/estante/1
     // /
-    def mostra(Integer max){
+    def mostra(Integer max) {
         def m = new Mensagem()
-        println 'ola'
         String fileLabel = params.fileLabel
         MultipartFile uploadedFile = null
-        String fileName=""
-        try{
-            if (request instanceof MultipartHttpServletRequest){
+        String fileName = ""
+        try {
+            if (request instanceof MultipartHttpServletRequest) {
                 //Get the file's name from request
-                fileName = request.getFileNames()[0]
+                //fileName = request.getFileNames()[0]
                 //Get a reference to the uploaded file.
-                uploadedFile = request.getFile(fileName)
-                fileLabel +=".${uploadedFile.originalFilename.split("\\.")[-1]}"
+                //get the file storage location
+                def fileTobeStoredInDirPath = '/home/ctic/Pictures/'
+                fileName = request.getFileNames()
+                //fileName = request.getMultiFileMap()
 
-            }
-            //get uploaded file's inputStream
-            InputStream inputStream = uploadedFile.inputStream
-            //get the file storage location
-            def fileTobeStoredInDirPath = '/home/proeg/Pictures/' ///grailsApplication.config.myapp.file.storage.location
-            //create a new file with fileLabel
-            File file = new File(fileTobeStoredInDirPath, fileLabel)
-            //This support both overriding and creating new file
-            //If two of these fails, that means got some internal issue. May be new file creation permissions issue
-            if (file.exists() || file.createNewFile()) {
+                request.fileNames.each {
+                    def nomeArquivo = it
+                    uploadedFile = request.getFile( nomeArquivo )
+                    fileLabel = uploadedFile.getOriginalFilename()
 
-                //to close the fileOutStream, opening it using withOutStream closure
-                file.withOutputStream{fos->
-                    IOUtils.copyLarge(inputStream, fos)
+                    //get uploaded file's inputStream
+                    InputStream inputStream = uploadedFile.inputStream
+                    //create a new file with fileLabel
+                    File file = new File(fileTobeStoredInDirPath, fileLabel)
+                    //This support both overriding and creating new file
+                    //If two of these fails, that means got some internal issue. May be new file creation permissions issue
+                    if (file.exists() || file.createNewFile()) {
+                        //to close the fileOutStream, opening it using withOutStream closure
+                        file.withOutputStream { fos ->
+                            IOUtils.copyLarge(inputStream, fos)
+                        }
+                    } else {
+                        m.codigo = '410'
+                        m.descricao = "Aconteceu um erro ao enviar o arquivo  ${file} no servidor ${fileTobeStoredInDirPath}"
+                    }
+                    m.codigo = '200'
+                    m.descricao = 'Arquivo importado com sucessso'
                 }
 
-                m.codigo = '200'
-                m.descricao = 'Arquivo importado com sucessso'
 
-            }else{
-                m.codigo = '410'
-                m.descricao = "Aconteceu um erro ao enviar o arquivo  ${file} no servidor ${fileTobeStoredInDirPath}"
             }
+
         }
-        catch (Exception e){
+        catch (Exception e) {
             m.codigo = '500'
             m.descricao = "Erro ao enviar os arquivos ${e.message}"
 
